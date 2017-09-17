f7d1c2fb1eae1961a569506d36e1cd3a7fe068a3
Tnifey <tnifey@gmail.com>
2017-09-17 21:30:01 +0200

    make dropdown great again

--- ui/scripts/init/dropdown.js
+++ ui/scripts/init/dropdown.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
 
+	$('[data-dropdown]').each(function () {
+		var t = $(this);
+		// dropdown
+		var did = t.attr('data-dropdown');
+		var d = $('#'+did);
+
+		var dp = d.attr('data-placement') || "bottom-start";
+
+		if(!window.dropdowns)
+			window.dropdowns = [];
+
+		var p = new Popper(t[0], d[0], {
+			placement: dp,
+			modifiers: {
+				preventOverflow: {
+					enabled: true,
+					boundariesElement: document.body,
+				},
+				flip: {
+					enabled: true,
+		            behavior: ['bottom-end']
+		        },
+			},
+		});
+
+		window.dropdowns[did] = p;
+
+		// d.hide();
+		setTimeout(function () {
+			d.hide();
+		}, 50)
+	});
+
 	$('[data-dropdown]').on('click', function (e) {
 		e.preventDefault();
 		var t = $(this);
@@ -7,16 +40,16 @@ $(document).ready(function () {
 		var did = t.attr('data-dropdown');
 		var d = $('#'+did);
 
+		window.dropdowns[did].update()
+
 		t.toggleClass('focus');
 
-		if(!t.hasClass('focus'))
+		if(!t.hasClass('focus')) {
 			t.blur();
-
-		d.toggleClass('dropdown-show');
-
-		var p = new Popper(t[0], d[0], {
-			placement: "bottom-end"
-		});
+			d.fadeOut(150);
+		} else {
+			d.fadeIn(150);
+		}
 	});
 
 });
